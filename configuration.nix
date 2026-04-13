e3a7f6a3a2b74a882e56adfbc49de017a7f6dcf6
Sacolle <pedro.h.b.colle@gmail.com>
2026-04-13 00:32:38 +0000

    removido config desnecessária

--- configuration.nix
+++ configuration.nix
@@ -140,13 +140,6 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    #neovim
-
-    auto-multiple-choice
-    (texlive.combine {
-      inherit (pkgs.texlive) scheme-full;
-      inherit auto-multiple-choice;
-    })    
   ];
 
 
